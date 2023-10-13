516fc17e9fb92507c502fcc8ee67c594b064193b
Nikita Sechnoy <NikitaSech@yandex.ru>
2023-10-13 21:23:50 +0300

    Выделит список предложений в отдельный компонент

---
+++ project/src/components/offers-list/offers-list.tsx
@@ -0,0 +1,40 @@
+import { Offer } from '../../types/offer';
+import OfferCard from '../place-card/offer-card';
+
+type OffersListProps = {
+  offers: Offer[];
+}
+
+export default function OffersList({offers}: OffersListProps): JSX.Element {
+  return (
+    <div className="cities">
+      <div className="cities__places-container container">
+        <section className="cities__places places">
+          <h2 className="visually-hidden">Places</h2>
+          <b className="places__found">{offers.length} places to stay in Amsterdam</b>
+          <form className="places__sorting" action="#" method="get">
+            <span className="places__sorting-caption">Sort by</span>
+            <span className="places__sorting-type" tabIndex={0}>
+              Popular
+              <svg className="places__sorting-arrow" width="7" height="4">
+                <use xlinkHref="#icon-arrow-select"></use>
+              </svg>
+            </span>
+            <ul className="places__options places__options--custom places__options--opened">
+              <li className="places__option places__option--active" tabIndex={0}>Popular</li>
+              <li className="places__option" tabIndex={0}>Price: low to high</li>
+              <li className="places__option" tabIndex={0}>Price: high to low</li>
+              <li className="places__option" tabIndex={0}>Top rated first</li>
+            </ul>
+          </form>
+          <div className="cities__places-list places__list tabs__content">
+            {offers.map((offer) => <OfferCard key={offer.id} offer={offer} />)}
+          </div>
+        </section>
+        <div className="cities__right-section">
+          <section className="cities__map map"></section>
+        </div>
+      </div>
+    </div>
+  );
+}
--- project/src/pages/main-screen/main-screen.tsx
+++ project/src/pages/main-screen/main-screen.tsx
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom';
-import OfferCard from '../../components/place-card/offer-card';
 import { AppRoute } from '../../const';
 import { Offer } from '../../types/offer';
+import OffersList from '../../components/offers-list/offers-list';
 
 type MainScreenProps = {
   offers: Offer[];
@@ -76,35 +76,7 @@ export default function MainScreen({ offers = [] }: MainScreenProps): JSX.Elemen
             </ul>
           </section>
         </div>
-        <div className="cities">
-          <div className="cities__places-container container">
-            <section className="cities__places places">
-              <h2 className="visually-hidden">Places</h2>
-              <b className="places__found">{offers.length} places to stay in Amsterdam</b>
-              <form className="places__sorting" action="#" method="get">
-                <span className="places__sorting-caption">Sort by</span>
-                <span className="places__sorting-type" tabIndex={0}>
-                  Popular
-                  <svg className="places__sorting-arrow" width="7" height="4">
-                    <use xlinkHref="#icon-arrow-select"></use>
-                  </svg>
-                </span>
-                <ul className="places__options places__options--custom places__options--opened">
-                  <li className="places__option places__option--active" tabIndex={0}>Popular</li>
-                  <li className="places__option" tabIndex={0}>Price: low to high</li>
-                  <li className="places__option" tabIndex={0}>Price: high to low</li>
-                  <li className="places__option" tabIndex={0}>Top rated first</li>
-                </ul>
-              </form>
-              <div className="cities__places-list places__list tabs__content">
-                {offers.map((offer) => <OfferCard key={offer.id} offer={offer} />)}
-              </div>
-            </section>
-            <div className="cities__right-section">
-              <section className="cities__map map"></section>
-            </div>
-          </div>
-        </div>
+        <OffersList offers={offers} />
       </main>
     </div>
   );
