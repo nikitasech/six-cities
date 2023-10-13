c8ccb02cd8f0a887698ea8c12135a68d5acd8677
Nikita Sechnoy <NikitaSech@yandex.ru>
2023-10-13 18:54:42 +0300

    Добавит моковые данные предложений. WIP
    WIP: Ошибка, так как модуль с моками не подключен

---
+++ project/src/mocks/offers.ts
@@ -0,0 +1,219 @@
+import { Offer } from '../types/offer';
+
+export const offers: Offer[] = [
+  {
+    city: {
+      name: 'Paris',
+      location: {
+        latitude: 48.85661,
+        longitude: 2.351499,
+        zoom: 13
+      }
+    },
+    previewImage: 'https://10.react.pages.academy/static/hotel/2.jpg',
+    images: [
+      'https://10.react.pages.academy/static/hotel/13.jpg',
+      'https://10.react.pages.academy/static/hotel/3.jpg',
+      'https://10.react.pages.academy/static/hotel/4.jpg',
+      'https://10.react.pages.academy/static/hotel/9.jpg',
+      'https://10.react.pages.academy/static/hotel/2.jpg',
+      'https://10.react.pages.academy/static/hotel/18.jpg',
+      'https://10.react.pages.academy/static/hotel/11.jpg',
+      'https://10.react.pages.academy/static/hotel/8.jpg',
+      'https://10.react.pages.academy/static/hotel/1.jpg',
+      'https://10.react.pages.academy/static/hotel/14.jpg',
+      'https://10.react.pages.academy/static/hotel/6.jpg',
+      'https://10.react.pages.academy/static/hotel/17.jpg',
+      'https://10.react.pages.academy/static/hotel/15.jpg',
+      'https://10.react.pages.academy/static/hotel/16.jpg'
+    ],
+    title: 'Waterfront with extraordinary view',
+    isFavorite: false,
+    isPremium: false,
+    rating: 2.7,
+    type: 'house',
+    bedrooms: 1,
+    maxAdults: 7,
+    price: 191,
+    goods: [
+      'Breakfast',
+      'Laptop friendly workspace'
+    ],
+    host: {
+      id: 25,
+      name: 'Angelina',
+      isPro: true,
+      avatarUrl: 'img/avatar-angelina.jpg'
+    },
+    description: 'This is a place for dreamers to reset, reflect, and create. Designed with a \'slow\' pace in mind, our hope is that you enjoy every part of your stay; from making local coffee by drip in the morning, choosing the perfect record to put on as the sun sets.',
+    location: {
+      latitude: 48.837610000000005,
+      longitude: 2.364499,
+      zoom: 16
+    },
+    id: 4
+  },
+  {
+    city: {
+      name: 'Brussels',
+      location: {
+        latitude: 50.846557,
+        longitude: 4.351697,
+        zoom: 13
+      }
+    },
+    previewImage: 'https://10.react.pages.academy/static/hotel/17.jpg',
+    images: [
+      'https://10.react.pages.academy/static/hotel/19.jpg',
+      'https://10.react.pages.academy/static/hotel/1.jpg',
+      'https://10.react.pages.academy/static/hotel/10.jpg',
+      'https://10.react.pages.academy/static/hotel/5.jpg',
+      'https://10.react.pages.academy/static/hotel/2.jpg',
+      'https://10.react.pages.academy/static/hotel/14.jpg',
+      'https://10.react.pages.academy/static/hotel/4.jpg',
+      'https://10.react.pages.academy/static/hotel/15.jpg',
+      'https://10.react.pages.academy/static/hotel/3.jpg',
+      'https://10.react.pages.academy/static/hotel/18.jpg',
+      'https://10.react.pages.academy/static/hotel/12.jpg',
+      'https://10.react.pages.academy/static/hotel/6.jpg',
+      'https://10.react.pages.academy/static/hotel/13.jpg',
+      'https://10.react.pages.academy/static/hotel/16.jpg'
+    ],
+    title: 'The house among olive ',
+    isFavorite: false,
+    isPremium: false,
+    rating: 2.1,
+    type: 'room',
+    bedrooms: 1,
+    maxAdults: 3,
+    price: 109,
+    goods: [
+      'Breakfast',
+      'Laptop friendly workspace'
+    ],
+    host: {
+      id: 25,
+      name: 'Angelina',
+      isPro: true,
+      avatarUrl: 'img/avatar-angelina.jpg'
+    },
+    description: 'A new spacious villa, one floor. All commodities, jacuzzi and beautiful scenery. Ideal for families or friends.',
+    location: {
+      latitude: 50.867557,
+      longitude: 4.357697,
+      zoom: 16
+    },
+    id: 73
+  },
+  {
+    city: {
+      name: 'Hamburg',
+      location: {
+        latitude: 53.550341,
+        longitude: 10.000654,
+        zoom: 13
+      }
+    },
+    previewImage: 'https://10.react.pages.academy/static/hotel/19.jpg',
+    images: [
+      'https://10.react.pages.academy/static/hotel/12.jpg',
+      'https://10.react.pages.academy/static/hotel/7.jpg',
+      'https://10.react.pages.academy/static/hotel/13.jpg',
+      'https://10.react.pages.academy/static/hotel/18.jpg',
+      'https://10.react.pages.academy/static/hotel/16.jpg',
+      'https://10.react.pages.academy/static/hotel/2.jpg',
+      'https://10.react.pages.academy/static/hotel/1.jpg',
+      'https://10.react.pages.academy/static/hotel/14.jpg',
+      'https://10.react.pages.academy/static/hotel/8.jpg',
+      'https://10.react.pages.academy/static/hotel/20.jpg',
+      'https://10.react.pages.academy/static/hotel/19.jpg',
+      'https://10.react.pages.academy/static/hotel/15.jpg',
+      'https://10.react.pages.academy/static/hotel/5.jpg',
+      'https://10.react.pages.academy/static/hotel/17.jpg'
+    ],
+    title: 'Waterfront with extraordinary view',
+    isFavorite: false,
+    isPremium: false,
+    rating: 2.5,
+    type: 'apartment',
+    bedrooms: 3,
+    maxAdults: 10,
+    price: 281,
+    goods: [
+      'Breakfast',
+      'Laptop friendly workspace',
+      'Washer'
+    ],
+    host: {
+      id: 25,
+      name: 'Angelina',
+      isPro: true,
+      avatarUrl: 'img/avatar-angelina.jpg'
+    },
+    description: 'Discover daily local life in city center, friendly neighborhood, clandestine casino, karaoke, old-style artisans, art gallery and artist studio downstairs.',
+    location: {
+      latitude: 53.573341000000006,
+      longitude: 9.994654,
+      zoom: 16
+    },
+    id: 76
+  },
+  {
+    city: {
+      name: 'Brussels',
+      location: {
+        latitude: 50.846557,
+        longitude: 4.351697,
+        zoom: 13
+      }
+    },
+    previewImage: 'https://10.react.pages.academy/static/hotel/8.jpg',
+    images: [
+      'https://10.react.pages.academy/static/hotel/18.jpg',
+      'https://10.react.pages.academy/static/hotel/3.jpg',
+      'https://10.react.pages.academy/static/hotel/2.jpg',
+      'https://10.react.pages.academy/static/hotel/9.jpg',
+      'https://10.react.pages.academy/static/hotel/14.jpg',
+      'https://10.react.pages.academy/static/hotel/6.jpg',
+      'https://10.react.pages.academy/static/hotel/19.jpg',
+      'https://10.react.pages.academy/static/hotel/15.jpg',
+      'https://10.react.pages.academy/static/hotel/5.jpg',
+      'https://10.react.pages.academy/static/hotel/1.jpg',
+      'https://10.react.pages.academy/static/hotel/17.jpg',
+      'https://10.react.pages.academy/static/hotel/12.jpg',
+      'https://10.react.pages.academy/static/hotel/4.jpg',
+      'https://10.react.pages.academy/static/hotel/13.jpg'
+    ],
+    title: 'The Pondhouse - A Magical Place',
+    isFavorite: false,
+    isPremium: false,
+    rating: 4.7,
+    type: 'room',
+    bedrooms: 1,
+    maxAdults: 1,
+    price: 182,
+    goods: [
+      'Breakfast',
+      'Towels',
+      'Laptop friendly workspace',
+      'Washer',
+      'Fridge',
+      'Dishwasher',
+      'Air conditioning',
+      'Baby seat'
+    ],
+    host: {
+      id: 25,
+      name: 'Angelina',
+      isPro: true,
+      avatarUrl: 'img/avatar-angelina.jpg'
+    },
+    description: 'This is a place for dreamers to reset, reflect, and create. Designed with a \'slow\' pace in mind, our hope is that you enjoy every part of your stay; from making local coffee by drip in the morning, choosing the perfect record to put on as the sun sets.',
+    location: {
+      latitude: 50.860557,
+      longitude: 4.376697,
+      zoom: 16
+    },
+    id: 8
+  }
+];
