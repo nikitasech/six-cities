9460e314b16b5864f228a945185970f4e7f9aff2
Nikita Sechnoy <NikitaSech@yandex.ru>
2023-10-20 17:41:32 +0300

    Добавит компонент ввода рейтинга и валидацию формы комментария

--- project/src/components/form-review/form-review.tsx
+++ project/src/components/form-review/form-review.tsx
@@ -1,68 +1,53 @@
 import { ChangeEvent, useState } from 'react';
+import ReviewRating from '../review-rating/review-rating';
+
+const MIN_REVIEW_LENGTH = 50;
+const MAX_REVIEW_LENGTH = 300;
+
+type FormData = {
+  rating: string;
+  review: string;
+  isValidity: boolean;
+}
 
 export default function FormReview(): JSX.Element {
   const [formData, setFormData] = useState({
     rating: '',
-    review: ''
+    review: '',
+    isValidity: false
   });
+
+  const getIsValidity = ({rating, review}: FormData) => {
+    const isRatingValidity = Boolean(rating.length);
+    const isReviewValidity = (
+      review.length >= MIN_REVIEW_LENGTH
+      && review.length <= MAX_REVIEW_LENGTH
+    );
+
+    return isRatingValidity && isReviewValidity;
+  };
 
   const handleFormDataChange = (
     evt: ChangeEvent<HTMLInputElement|HTMLTextAreaElement>
   ) => {
-    evt.preventDefault();
-
     const {name, value} = evt.target;
     setFormData({
       ...formData,
-      [name]: value
+      [name]: value,
+      isValidity: getIsValidity({...formData, [name]: value})
     });
   };
 
   return (
     <form className="reviews__form form" action="#" method="post">
       <label className="reviews__label form__label" htmlFor="review">Your review</label>
-      <div className="reviews__rating-form form__rating">
-        <input className="form__rating-input visually-hidden" name="rating" value="5" id="5-stars" type="radio" onChange={handleFormDataChange} />
-        <label htmlFor="5-stars" className="reviews__rating-label form__rating-label" title="perfect">
-          <svg className="form__star-image" width="37" height="33">
-            <use xlinkHref="#icon-star"></use>
-          </svg>
-        </label>
-
-        <input className="form__rating-input visually-hidden" name="rating" value="4" id="4-stars" type="radio" onChange={handleFormDataChange} />
-        <label htmlFor="4-stars" className="reviews__rating-label form__rating-label" title="good">
-          <svg className="form__star-image" width="37" height="33">
-            <use xlinkHref="#icon-star"></use>
-          </svg>
-        </label>
-
-        <input className="form__rating-input visually-hidden" name="rating" value="3" id="3-stars" type="radio" onChange={handleFormDataChange} />
-        <label htmlFor="3-stars" className="reviews__rating-label form__rating-label" title="not bad">
-          <svg className="form__star-image" width="37" height="33">
-            <use xlinkHref="#icon-star"></use>
-          </svg>
-        </label>
-
-        <input className="form__rating-input visually-hidden" name="rating" value="2" id="2-stars" type="radio" onChange={handleFormDataChange} />
-        <label htmlFor="2-stars" className="reviews__rating-label form__rating-label" title="badly">
-          <svg className="form__star-image" width="37" height="33">
-            <use xlinkHref="#icon-star"></use>
-          </svg>
-        </label>
-
-        <input className="form__rating-input visually-hidden" name="rating" value="1" id="1-star" type="radio" onChange={handleFormDataChange} />
-        <label htmlFor="1-star" className="reviews__rating-label form__rating-label" title="terribly">
-          <svg className="form__star-image" width="37" height="33">
-            <use xlinkHref="#icon-star"></use>
-          </svg>
-        </label>
-      </div>
+      <ReviewRating onChange={handleFormDataChange} />
       <textarea className="reviews__textarea form__textarea" id="review" name="review" placeholder="Tell how was your stay, what you like and what can be improved" onChange={handleFormDataChange}></textarea>
       <div className="reviews__button-wrapper">
         <p className="reviews__help">
           To submit review please make sure to set <span className="reviews__star">rating</span> and describe your stay with at least <b className="reviews__text-amount">50 characters</b>.
         </p>
-        <button className="reviews__submit form__submit button" type="submit" disabled>Submit</button>
+        <button className="reviews__submit form__submit button" type="submit" disabled={!formData.isValidity}>Submit</button>
       </div>
     </form>
   );
