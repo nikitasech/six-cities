520b3507c90d12c05a27e3fe75075b9c395e3e51
Nikita Sechnoy <NikitaSech@yandex.ru>
2023-11-25 23:55:14 +0300

    feat: подключит изменение активного города по действию пользователя

--- project/src/components/cities/cities.tsx
+++ project/src/components/cities/cities.tsx
@@ -1,21 +1,27 @@
 import { Link } from 'react-router-dom';
 import { AppRoute, cities } from '../../const';
+import { useAppSelector } from '../../hooks/use-app-selector';
+import { useAppDispatch } from '../../hooks/use-app-dispatch';
+import { setActiveCity } from '../../store/actions';
+import { CityName } from '../../types/city-name';
 
 export default function Cities(): JSX.Element {
-  const activeCity = cities[3];
+  const dispatch = useAppDispatch();
+  const activeCity = useAppSelector((state) => state.activeCity);
+  const handleCityClick = (name: CityName) => () => dispatch(setActiveCity(name));
 
   return (
     <section className="locations container">
       <div className="tabs">
         <ul className="locations__list tabs__list">
           {cities.map((city) => {
-            const activeClasses = activeCity === city
+            const activeClass = activeCity === city
               ? 'tabs__item--active'
               : '';
 
             return (
               <li className="locations__item" key={city}>
-                <Link className={`locations__item-link tabs__item' ${activeClasses}`} to={AppRoute.Root}>
+                <Link className={`locations__item-link tabs__item' ${activeClass}`} to={AppRoute.Root} onClick={handleCityClick(city)}>
                   <span>{city}</span>
                 </Link>
               </li>
