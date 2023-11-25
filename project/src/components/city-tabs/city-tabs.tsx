b3526214bd55c82482a6b70bf33ef5fb949169bd
Nikita Sechnoy <NikitaSech@yandex.ru>
2023-11-26 01:18:43 +0300

    refactor: подключит универсальный компонент CityTab в CityTabs

--- project/src/components/city-tabs/city-tabs.tsx
+++ project/src/components/city-tabs/city-tabs.tsx
@@ -1,31 +1,17 @@
-import { Link } from 'react-router-dom';
-import { AppRoute, cities } from '../../const';
+import { cities } from '../../const';
 import { useAppSelector } from '../../hooks/use-app-selector';
-import { useAppDispatch } from '../../hooks/use-app-dispatch';
-import { setActiveCity } from '../../store/actions';
-import { CityName } from '../../types/city-name';
+import CityTab from '../city-tab/city-tab';
 
 export default function CityTabs(): JSX.Element {
-  const dispatch = useAppDispatch();
   const activeCity = useAppSelector((state) => state.activeCity);
-  const handleCityClick = (name: CityName) => () => dispatch(setActiveCity(name));
 
   return (
     <section className="locations container">
       <div className="tabs">
         <ul className="locations__list tabs__list">
           {cities.map((city) => {
-            const activeClass = activeCity === city
-              ? 'tabs__item--active'
-              : '';
-
-            return (
-              <li className="locations__item" key={city}>
-                <Link className={`locations__item-link tabs__item' ${activeClass}`} to={AppRoute.Root} onClick={handleCityClick(city)}>
-                  <span>{city}</span>
-                </Link>
-              </li>
-            );
+            const isActiveTab = activeCity === city;
+            return <CityTab key={city} name={city} isActive={isActiveTab} />;
           })}
         </ul>
       </div>
