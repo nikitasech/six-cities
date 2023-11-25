d71cada51228d8880a62958563c91f3c2a503432
Nikita Sechnoy <NikitaSech@yandex.ru>
2023-11-25 23:28:27 +0300

    feat: добавит и подключит глобальное хранилище

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 import { offers } from './mocks/offers';
 import { city } from './mocks/city';
+import { Provider } from 'react-redux';
+import { store } from './store/store';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
@@ -10,6 +12,8 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <App city={city} offers={offers} />
+    <Provider store={store}>
+      <App city={city} offers={offers} />
+    </Provider>
   </React.StrictMode>,
 );
