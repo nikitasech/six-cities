f37dc1221f40f7d801baada0665f4526757488c2
Nikita Sechnoy <NikitaSech@yandex.ru>
2023-11-26 13:23:08 +0300

    feat: опишет получение предложений в хранилище

--- project/src/store/actions.ts
+++ project/src/store/actions.ts
@@ -1,4 +1,6 @@
 import { createAction } from '@reduxjs/toolkit';
 import { CityName } from '../types/city-name';
+import { Offer } from '../types/offer';
 
 export const setActiveCity = createAction<CityName>('setActiveCity');
+export const setOffers = createAction<Offer[]>('setOffers');
--- project/src/store/reducer.ts
+++ project/src/store/reducer.ts
@@ -1,19 +1,25 @@
 import { createReducer } from '@reduxjs/toolkit';
 import { cities } from '../const';
-import { setActiveCity } from './actions';
+import { setActiveCity, setOffers } from './actions';
 import { CityName } from '../types/city-name';
+import { Offer } from '../types/offer';
 
 type State = {
   activeCity: CityName;
+  offers: Offer[];
 }
 
 const initialState: State = {
   activeCity: cities[0],
+  offers: []
 };
 
 export const reducer = createReducer(initialState, (builder) => {
   builder
     .addCase(setActiveCity, (state, action) => {
       state.activeCity = action.payload;
+    })
+    .addCase(setOffers, (state, action) => {
+      state.offers = action.payload;
     });
 });
