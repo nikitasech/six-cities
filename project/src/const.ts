e27a4228e9ca669cbbbb5a5ff50aece51f38b4d8
Nikita Sechnoy <NikitaSech@yandex.ru>
2023-12-03 23:32:28 +0300

    feat: создаст объект городов с координатами центра

--- project/src/const.ts
+++ project/src/const.ts
@@ -1,3 +1,6 @@
+import { CityName } from './types/city-name';
+import { Location } from './types/location';
+
 export const cities: string[] = ['Paris', 'Cologne', 'Brussels', 'Amsterdam', 'Hamburg', 'Dusseldorf'];
 
 export enum AppRoute {
@@ -12,3 +15,36 @@ export enum AuthStatus {
   NoAuth = 'NO_AUTH',
   Unknown = 'UNKNOWN'
 }
+
+export const CityLocation: { [key in CityName]: Location } = {
+  'Paris': {
+    latitude: 48.85661,
+    longitude: 2.351499,
+    zoom: 12
+  },
+  'Cologne': {
+    latitude: 50.938361,
+    longitude: 6.959974,
+    zoom: 12
+  },
+  'Brussels': {
+    latitude: 50.846557,
+    longitude: 4.351697,
+    zoom: 12
+  },
+  'Amsterdam': {
+    latitude: 52.37454,
+    longitude: 4.897976,
+    zoom: 12
+  },
+  'Hamburg': {
+    latitude: 53.550341,
+    longitude: 10.000654,
+    zoom: 12
+  },
+  'Dusseldorf': {
+    latitude: 51.225402,
+    longitude: 6.776314,
+    zoom: 12
+  },
+};
