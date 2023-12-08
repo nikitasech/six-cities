6d70a2ba4325e8dc69fe6fc6d250b5028596fe22
Nikita Sechnoy <NikitaSech@yandex.ru>
2023-12-08 19:07:00 +0300

    refactor: выделит сортировку в отдельный компонент

--- project/src/components/offers-list/offers-list.tsx
+++ project/src/components/offers-list/offers-list.tsx
@@ -3,6 +3,7 @@ import { Offer } from '../../types/offer';
 import OfferCard from '../offer-card/offer-card';
 import Map from '../map/map';
 import { useAppSelector } from '../../hooks/use-app-selector';
+import Sort from '../sort/sort';
 
 export default function OffersList(): JSX.Element {
   const activeCity = useAppSelector((state) => state.activeCity);
@@ -16,21 +17,7 @@ export default function OffersList(): JSX.Element {
         <section className="cities__places places">
           <h2 className="visually-hidden">Places</h2>
           <b className="places__found">{offers.length} places to stay in Amsterdam</b>
-          <form className="places__sorting" action="#" method="get">
-            <span className="places__sorting-caption">Sort by</span>
-            <span className="places__sorting-type" tabIndex={0}>
-              Popular
-              <svg className="places__sorting-arrow" width="7" height="4">
-                <use xlinkHref="#icon-arrow-select"></use>
-              </svg>
-            </span>
-            <ul className="places__options places__options--custom places__options--opened">
-              <li className="places__option places__option--active" tabIndex={0}>Popular</li>
-              <li className="places__option" tabIndex={0}>Price: low to high</li>
-              <li className="places__option" tabIndex={0}>Price: high to low</li>
-              <li className="places__option" tabIndex={0}>Top rated first</li>
-            </ul>
-          </form>
+          <Sort />
           <div className="cities__places-list places__list tabs__content">
             {offers.map((offer) => (
               <OfferCard
