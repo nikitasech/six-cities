61e53c5a91146e0ee44d44fb39d13895c53fd927
Nikita Sechnoy <NikitaSech@yandex.ru>
2023-12-08 23:37:44 +0300

    feat: добавит функцию открытия/закрытия

--- project/src/components/sort/sort.tsx
+++ project/src/components/sort/sort.tsx
@@ -1,14 +1,27 @@
+import cn from 'classnames';
+import { useState } from 'react';
+
 export default function Sort(): JSX.Element {
+  const [isOpened, setIsOpened] = useState<boolean>(false);
+
+  const listClasses = cn('places__options places__options--custom', {
+    'places__options--opened': isOpened
+  });
+
+  const handleSortClick = () => {
+    setIsOpened(!isOpened);
+  };
+
   return (
     <form className="places__sorting" action="#" method="get">
       <span className="places__sorting-caption">Sort by</span>
-      <span className="places__sorting-type" tabIndex={0}>
+      <span className="places__sorting-type" tabIndex={0} onClick={handleSortClick}>
         Popular
         <svg className="places__sorting-arrow" width="7" height="4">
           <use xlinkHref="#icon-arrow-select"></use>
         </svg>
       </span>
-      <ul className="places__options places__options--custom places__options--opened">
+      <ul className={listClasses}>
         <li className="places__option places__option--active" tabIndex={0}>Popular</li>
         <li className="places__option" tabIndex={0}>Price: low to high</li>
         <li className="places__option" tabIndex={0}>Price: high to low</li>
