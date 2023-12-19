74ff05baafe7e68cac269e667eb3c4fc46ab3fc0
Nikita Sechnoy <NikitaSech@yandex.ru>
2023-12-20 02:39:43 +0300

    feat: добавит действие logout

--- project/src/store/thunk-actions.ts
+++ project/src/store/thunk-actions.ts
@@ -2,14 +2,14 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import type { AppDispatch } from '../types/store';
 import type { AxiosInstance } from 'axios';
 import type { Offer } from '../types/offer';
-import { setAuthStatus, setIsLoading, setOffers } from './actions';
-import type { State } from './state';
+import { setAuthStatus, setIsLoading, setOffers, setUser } from './actions';
 import { AuthStatus, ServerRoute } from '../const';
 import { User } from '../types/user';
+import { setToken } from '../features/token';
+import { LoginData } from '../types/login-data';
 
 export const fetchOffers = createAsyncThunk<void, undefined, {
   dispatch: AppDispatch;
-  state: State;
   extra: AxiosInstance;
 }>(
   'fetchOffers',
@@ -24,7 +24,6 @@ export const fetchOffers = createAsyncThunk<void, undefined, {
 
 export const checkAuthStatus = createAsyncThunk<void, undefined, {
   dispatch: AppDispatch;
-  state: State;
   extra: AxiosInstance;
 }>(
   'checkAuthStatus',
@@ -38,4 +37,19 @@ export const checkAuthStatus = createAsyncThunk<void, undefined, {
       });
 
   },
+);
+
+export const login = createAsyncThunk<void, LoginData, {
+  dispatch: AppDispatch;
+  extra: AxiosInstance;
+}>(
+  'login',
+  async (loginData, {dispatch, extra: api}) => {
+    await api.post<User>(ServerRoute.Login, loginData)
+      .then((response) => {
+        setToken(response.data.token);
+        dispatch(setAuthStatus(AuthStatus.Auth));
+        dispatch(setUser(response.data));
+      });
+  }
 );
