56516b5d2e86aeb9fc2a1803005ca086dba96558
Nikita Sechnoy <NikitaSech@yandex.ru>
2023-12-19 22:20:19 +0300

    feat: добавит отображение навигации шапки в зависимости от авторизации

--- project/src/components/header-nav/header-nav.tsx
+++ project/src/components/header-nav/header-nav.tsx
@@ -1,7 +1,18 @@
 import { Link } from 'react-router-dom';
-import { AppRoute } from '../../const';
+import { AppRoute, AuthStatus } from '../../const';
+import { useAppSelector } from '../../hooks/use-app-selector';
 
 export default function HeaderNav(): JSX.Element {
+  const authStatus = useAppSelector((state) => state.authStatus);
+
+  const userSpanClasses = authStatus === AuthStatus.Auth
+    ? 'header__user-name user__name'
+    : 'header__login';
+
+  const userSpanContent = authStatus === AuthStatus.Auth
+    ? 'Oliver.conner@gmail.com' // #TODO заменить на реальный email пользователя
+    : 'Sign in';
+
   return (
     <nav className="header__nav">
       <ul className="header__nav-list">
@@ -9,14 +20,18 @@ export default function HeaderNav(): JSX.Element {
           <Link className="header__nav-link header__nav-link--profile" to={AppRoute.Favorites}>
             <div className="header__avatar-wrapper user__avatar-wrapper">
             </div>
-            <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
+            <span className={userSpanClasses}>{userSpanContent}</span>
           </Link>
         </li>
-        <li className="header__nav-item">
-          <Link className="header__nav-link" to={window.location.pathname}>
-            <span className="header__signout">Sign out</span>
-          </Link>
-        </li>
+        {
+          authStatus === AuthStatus.Auth
+          &&
+          <li className="header__nav-item">
+            <Link className="header__nav-link" to={window.location.pathname}>
+              <span className="header__signout">Sign out</span>
+            </Link>
+          </li>
+        }
       </ul>
     </nav>
   );
