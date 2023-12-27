41a279bd72eec952a018de82039983b38da67278
Nikita Sechnoy <NikitaSech@yandex.ru>
2023-12-27 19:28:41 +0300

    refactor: мигрирует на последнюю версию react-router-dom, изменяет структуру проекта

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './components/app/app';
-import { city } from './mocks/city';
-import { Provider } from 'react-redux';
-import { store } from './store/store';
+import App from './app/app';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
@@ -11,8 +8,6 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App city={city} />
-    </Provider>
+    <App />
   </React.StrictMode>,
 );
